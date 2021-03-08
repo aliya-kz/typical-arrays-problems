69978d8b1fd3c2c9aa83254be2de638c5b1789f0
Aliya Zhumagulova <zhumagul100@gmail.com>
2021-03-08 15:10:09 +0600

    feat: update files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
-exports.min = function min (array) {
-  return 0;
+ exports.min=function min (array) {
+	if (array == null || array.length<1) {return 0; }
+	else {	
+	var minim=10000;
+
+	for (var i=0;i<array.length; i++){
+		array[i]<minim? minim=array[i] : minim=minim ;
+			}
+
+			
+  return minim;}
 }
+	
 
 exports.max = function max (array) {
-  return 0;
+	var maxi=0;
+	if (array == null || array.length<1) {return 0; }
+	else {
+	for (var i=0;i<array.length; i++){
+		array[i]>maxi? maxi=array[i] : maxi=maxi ;
+		
+	}
+  return maxi;
+}
 }
 
 exports.avg = function avg (array) {
-  return 0;
-}
+ if (array == null || array.length<1) {return 0; }
+ else{
+ var sum=0;
+ for (var i=0;i<array.length; i++){
+	sum=sum+array[i];
+	 
+ }
+return sum/array.length;
+}}
